ff26276858689c8a33ad5372ae2bd81d4636f27f
Owen Rubel <orubel@gmail.com>
2019-05-29 13:01:20 -0700

    removing files that change per build

--- skeleton/grails-app/conf/application.groovy
+++
@@ -1,9 +0,0 @@
-grails.config.locations = [
-    "file:${userHome}/.beapi/beapi_server.yml",
-	"file:${userHome}/.beapi/beapi.groovy",
-	"file:${userHome}/.beapi/beapi.yml",
-	"file:${userHome}/.beapi/beapi_db.yml",
-	"file:${userHome}/.beapi/beapi_api.yml",
-	"file:${userHome}/.beapi/beapi_messages.yml"
-]
-
