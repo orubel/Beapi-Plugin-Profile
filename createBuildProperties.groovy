fe5d2387f0d0b02b36c7b8553df1cf43b2b1891a
Owen Rubel <orubel@gmail.com>
2019-06-14 11:10:34 -0700

    adding file to auto-increment version in build pipeline

---
+++ createBuildProperties.groovy
@@ -0,0 +1,19 @@
+#!/usr/bin/env groovy
+
+String userHome = System.properties['user.home']
+
+Properties props = new Properties()
+def propsFile = new File("${userHome}/.jenkins/workspace/api-framework/gradle.properties")
+props.load(propsFile.newDataInputStream())
+def buildVersion = props.getProperty('buildVersion')
+def patchVersion = props.getProperty('patchVersion')
+def profileVersion = "${buildVersion}.${patchVersion}"
+
+Properties props2 = new Properties()
+def propsFile2 = new File("${userHome}/.jenkins/workspace/beapi-profile/gradle.properties")
+props2.load(propsFile2.newDataInputStream())
+
+props2.setProperty('version', profileVersion)
+props2.store(propsFile2.newWriter(), null)
+
+
