e4c28c85032b3488118e0d98479a4b559453bb0a
Owen Rubel <orubel@gmail.com>
2019-12-26 12:50:43 -0800

    updating CI scripts

--- createBuildProperties.groovy
+++ createBuildProperties.groovy
@@ -1,21 +1,14 @@
 #!/usr/bin/env groovy
 
-String userHome = System.properties['user.home']
-
 Properties props = new Properties()
-def propsFile = new File("${userHome}/.jenkins/workspace/api-framework/gradle.properties")
+def propsFile = new File("/var/lib/jenkins/workspace/api-framework/gradle.properties")
 props.load(propsFile.newDataInputStream())
 def buildVersion = props.getProperty('buildVersion')
 def patchVersion = props.getProperty('patchVersion')
 def profileVersion = "${buildVersion}.${patchVersion}"
 
-
 Properties props2 = new Properties()
-def propsFile2 = new File("${userHome}/.jenkins/workspace/beapi-profile/gradle.properties")
+def propsFile2 = new File("/var/lib/jenkins/workspace/beapi-profile/gradle.properties")
 props2.load(propsFile2.newDataInputStream())
 props2.setProperty('version', profileVersion)
 props2.store(propsFile2.newWriter(), null)
-
-
-//props.close()
-//props2.close()
